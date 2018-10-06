89433d1fd76c57b48bb96a10db74a60ecd337242
fatahrez <fertahrez@gmail.com>
2018-10-07 01:12:55 +0300

    add routing in src/main/java/App.java for index.vtl

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,13 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 public class App {
     public static void main(String [] args){
+        staticFileLocation("/public");
+        String layout = "templates/layout.vtl";
 
+        get("/", (request, response)->{
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/index.vtl");
+            return modelAndView(model, layout);
+        }, new VelocityTemplateEngine());
     }
 }
