38c23a983ec71916ce3a5133bc007e1f8168edd1
fatahrez <fertahrez@gmail.com>
2018-10-07 01:00:19 +0300

    import spark library and VelocityTemplateEngine in src/main/java/App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,8 @@
 import java.util.Map;
 import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 public class App {
     public static void main(String [] args){
 
