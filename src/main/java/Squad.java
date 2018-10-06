91c8f14a3b8a833fe8ec6dfc323cd0c74119d42a
fatahrez <fertahrez@gmail.com>
2018-10-06 23:38:14 +0300

    Test setMaximumSize in src/test/java/SquadTest.java passes so move to the next step

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Squad {
     List<Object> squadall = null;
-    // List<Object> squadall=null;
     public Squad(){
-         squadall= Arrays.asList(new Object[5]) ;
+        squadall = new ArrayList<Object>();
     }
     public void addToSquad(Hero newHero){
+        if(squadall.size() > 5){
+            throw new IllegalArgumentException("maximum size is 5");
+        }
+        else if(squadall.size() < 6){
             squadall.add(newHero);
+        }
     }
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -3,10 +3,30 @@ import static org.junit.Assert.*;
 
 public class SquadTest{
   @Test
-    public void squad_addNewHeroToSquad_Hero(){
+    public void squad_addNewHeroToSquad_Object(){
         Squad topOnes = new Squad();
         Hero superman = new Hero("superman", 34, "punch", "himself", 9, 10);
         topOnes.addToSquad(superman);
         assertTrue(topOnes.squadall.contains(superman));
+    }
+  @Test(expected = IllegalArgumentException.class)
+    public void squad_setMaximumSize_Object(){
+      Squad topOnes = new Squad();
+
+      Hero superman = new Hero("superman", 34, "punch", "himself", 9, 10);
+      Hero superwoman = new Hero("superwoamn", 31, "kick", "too slow", 7, 10);
+      Hero spiderman = new Hero("spider", 19, "web", "superwoman", 7, 10);
+      Hero batman = new Hero("batman", 39, "bats", "himself", 9, 10);
+      Hero batlady = new Hero("batlady", 26, "sleeping", "being awake", 5, 10);
+      Hero fertah = new Hero("superman", 34, "punch", "himself", 9, 10);
+      Hero you = new Hero("df", 98, "power", "weakness", 9, 10);
+
+      topOnes.addToSquad(superman);
+      topOnes.addToSquad(superwoman);
+      topOnes.addToSquad(spiderman);
+      topOnes.addToSquad(batman);
+      topOnes.addToSquad(batlady);
+      topOnes.addToSquad(fertah);
+      topOnes.addToSquad(you);
     }
 }
